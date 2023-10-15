c64d047bf25625a89c1b138a70f7c6b2b9c64549
JV <joaovitoraraujo1821@gmail.com>
2023-10-15 17:42:18 -0300

    Alteração Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,72 +1,81 @@
-import React, {useEffect, useState} from "react";
-import { StyleSheet, Text, View, SafeAreaView, Image} from "react-native";
-import {Card, CardMiddle} from "../components/Card.jsx";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import { Card, CardMiddle } from "../components/Card.jsx";
 import { useSelector } from 'react-redux';
 import { onCreateTriggerNotification } from "../services/notification.js";
 import { getList } from "../services/API.js";
 
 const Dashboard = () => {
-   const token = useSelector(state => state.authReducer.token);
-   const currentUser = useSelector(state => state.currentUser);
-   const [list, setList] = useState([]);
-   const currentDate = new Date().toLocaleDateString('en-GB', {timeZone: 'UTC'});
-   
-   const handleFirstName = (name) => {
-      return name.split(" ")[0];
-   }
+  const token = useSelector(state => state.authReducer.token);
+  const currentUser = useSelector(state => state.currentUser);
+  const currentDate = new Date().toLocaleDateString('en-GB', { timeZone: 'UTC' });
+  const firstName = currentUser.user.nome.split(" ")[0];
 
-   const handleNotify = async (nome, hora, minuto) => {
-       await onCreateTriggerNotification(nome, hora, minuto);
-   }  
+  const [list, setList] = useState([]);
+  const [nextMedication, setNextMedication] = useState(null);
 
-   const requestList = async () => {
-      try {
-          const response = await getList(token, currentDate);
-  
-          setList(response["horariosMedicamentos"]);
-   
-  
-      } catch(error) {
-          console.warn(error.message);
+  const handleNotify = async (nome, hora) => {
+    await onCreateTriggerNotification(nome, hora);
+  }
+
+  const requestList = async () => {
+    try {
+      const response = await getList(token, currentDate);
+      setList(response?.horariosMedicamentos || []);
+    } catch (error) {
+      console.warn(error.message);
+    }
+  }
+
+  useEffect(() => {
+    requestList();
+  }, [])
+
+  useEffect(() => {
+    if (list.length > 0) {
+      const now = new Date();
+      const currentTime = now.getHours() * 60 + now.getMinutes();
+      const upcomingMedications = list
+        .map(med => {
+          const [hour, minute] = med.horario.split(':').map(Number);
+          return { ...med, time: hour * 60 + minute };
+        })
+        .filter(med => med.time > currentTime);
+
+      if (upcomingMedications.length > 0) {
+        const nextMedication = upcomingMedications[0];
+        setNextMedication(nextMedication);
+        handleNotify(nextMedication.nome, nextMedication.horario);
       }
     }
+  }, [list]);
 
-   const firstName = handleFirstName(currentUser.user.nome);
+  const totalMedications = new Set(list).size;
 
-     useEffect(() => {
-       requestList();
-      
-     }, [])
-
-
-     list.map((med) => handleNotify(med.nome, parseInt(med.horario.substr(0, 2)), parseInt(med.horario.substr(3, 6))))
-     
-   
-     
-   return (
-    <SafeAreaView style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
-      
+  return (
+    <SafeAreaView style={style.container}>
       <View style={style.topSection}>
-         <Text style={style.title}>Olá, {firstName}</Text>
-         <Card title="Próximo medicamento" nome={`Prednisona`} hora={`15:30`}/>
+        <Text style={style.title}>Olá, {firstName}</Text>
+        {nextMedication && (
+          <Card title="Próximo medicamento" nome={nextMedication.nome} hora={nextMedication.horario} />
+        )}
       </View>
 
       <View style={style.middleSection}>
-         <Text style={style.title2}>Relatório de uso</Text>
-         <Image source={require('../assets/icons/icons8-graph-report-30.png')} style={style.img}></Image>
+        <Text style={style.title2}>Relatório de uso</Text>
+        <Image source={require('../assets/icons/icons8-graph-report-30.png')} style={style.img} />
       </View>
 
       <View style={style.cards}>
-         <CardMiddle title='Esquecimentos' msg='2'/>
-         <CardMiddle title='Medicamento mais esquecido' msg='Topiramato'/> 
-         <CardMiddle title='Total de medicamentos' msg="2"/>
-         <CardMiddle title='Frequência' msg="30 dias"/>
+        <CardMiddle title='Esquecimentos' msg='2' />
+        <CardMiddle title='Medicamento mais esquecido' msg='Topiramato' />
+        <CardMiddle title='Total de medicamentos' msg={totalMedications.toString()} />
+        <CardMiddle title='Frequência' msg="30 dias" />
       </View>
-        
     </SafeAreaView>
-   
-   )
+  )
 }
+
 
 const style = StyleSheet.create({
    title: {
